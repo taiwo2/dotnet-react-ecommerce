59cdb143e68b8f22575a69b9694b09b10aa869ab
taiwo2 <adetonat@ymail.com>
2023-07-20 23:32:26 +0100

    setup Database

---
+++ API/Data/DbInitializer.cs
@@ -0,0 +1,8 @@
+
+namespace API.Data
+{
+    public class DbInitializer
+    {
+        
+    }
+}
--- API/Entities/Product.cs
+++ API/Entities/Product.cs
@@ -6,7 +6,7 @@ namespace API.Entities
         public string Name { get; set; }
         public string Description { get; set; }
 
-        public int Dscription { get; set; }
+        public string Type { get; set; }
 
         public long Price{ get; set; }
         public string PictureUrl { get; set; }
