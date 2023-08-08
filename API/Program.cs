306e8d6c382e98555b351d0864472689c0c51db1
taiwo2 <adetonat@ymail.com>
2023-08-08 21:38:34 +0100

    Connect to client

--- API/Program.cs
+++ API/Program.cs
@@ -13,6 +13,8 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,8 +24,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.UseCors(opt =>
+{
+    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+});
 
+app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
