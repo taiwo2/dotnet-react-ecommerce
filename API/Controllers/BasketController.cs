5aa8fb931e084fe40d84e705fedaa8d5d1b22dbe
taiwo2 <adetonat@ymail.com>
2023-08-14 15:12:59 +0100

    Add Post API logic

--- API/Controllers/BasketController.cs
+++ API/Controllers/BasketController.cs
@@ -39,12 +39,41 @@ namespace API.Controllers
             };
         }
 
+        [HttpPost]
+        public async Task<ActionResult<BasketDto>> AddItemToBasket(int productId, int quantity)
+        {
+            var basket = await RetrieveBasket();
+            if (basket == null) basket = CreateBasket();
+
+            var product = await _context.Products.FindAsync(productId);
+
+            if (product == null) return NotFound();
+
+            basket.AddItem(product, quantity);
+
+            var result = await _context.SaveChangesAsync() > 0;
+
+            if (result) return CreatedAtRoute("GetBasket", MapBasketToDto(basket));
+
+            return BadRequest(new ProblemDetails { Title = "Problem saving item to basket" });
+        }
+
         private async Task<Basket> RetrieveBasket()
         {
             return await _context.Baskets
                 .Include(i => i.Items)
                 .ThenInclude(p => p.Product)
                 .FirstOrDefaultAsync(basket => basket.BuyerId == Request.Cookies["buyerId"]);
+        }
+
+        private Basket CreateBasket()
+        {
+            var buyerId = Guid.NewGuid().ToString();
+            var cookieOptions = new CookieOptions { IsEssential = true, Expires = DateTime.Now.AddDays(30) };
+            Response.Cookies.Append("buyerId", buyerId, cookieOptions);
+            var basket = new Basket { BuyerId = buyerId };
+            _context.Baskets.Add(basket);
+            return basket;
         }
     }
 }
