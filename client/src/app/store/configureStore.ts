61fc12a8dea6d9d2c03948934644ffb5e3914b12
taiwo2 <adetonat@ymail.com>
2023-10-14 19:49:44 +0100

    Add redux

---
+++ client/src/app/store/configureStore.ts
@@ -0,0 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { basketSlice } from "../../features/Basket/BasketSlice";
+
+
+
+export const store = configureStore({
+    reducer: {
+        basket: basketSlice.reducer,
+    }
+})
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
